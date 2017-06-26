aa9b92c0ae43e07f016ca9b4134d34f8aa273d63
Joseph Akroush <akroushj@gmail.com>
2017-06-25 20:23:50 -0400

    Create Program.java

---
+++ Data Structures/Linked Lists/Reverse Linked List/Program.java
@@ -0,0 +1,14 @@
+Node Reverse(Node head) {
+    Node current = head;
+    Node next = null;
+    Node previous = null;
+    
+    while (current != null) {
+        next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+    }
+    
+    return previous;
+}
